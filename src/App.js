06dcc08442538cf624491e5e430bd0775307f248
eylee8714 <eylee8714@gmail.com>
2021-05-12 19:29:44 +0900

    Input onChange using react-hook

--- src/App.js
+++ src/App.js
@@ -2,11 +2,19 @@ import React, { useState } from 'react';
 
 function App() {
   const [count, setCount] = useState(0);
+  const [email, setEmail] = useState('');
+  const updateEmail = (e) => {
+    const {
+      target: { value },
+    } = e;
+    setEmail(value);
+  };
   return (
     <>
       {count}
       <button onClick={() => setCount(count + 1)}>Increment</button>
       <button onClick={() => setCount(count - 1)}>Decrement</button>
+      <input placeholder="Email" value={email} onChange={updateEmail} />
     </>
   );
 }
