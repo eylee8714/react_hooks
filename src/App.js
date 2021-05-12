72fcf3ea309625315a3c3d201a3dc97995b95ed0
eylee8714 <eylee8714@gmail.com>
2021-05-12 19:21:57 +0900

    Function Component with Increment Button

--- src/App.js
+++ src/App.js
@@ -1,31 +1,14 @@
-import logo from './logo.svg';
-import './App.css';
-import { Component } from 'react';
+import React, { useState } from 'react';
 
-class App extends Component {
-  state = { count: 0 };
-
-  increment = () => {
-    this.setState((current) => {
-      return {
-        count: current + 1,
-      };
-    });
-  };
-
-  modify = (n) => {
-    this.setState({ count: n });
-  };
-
-  render() {
-    const { count } = this.state;
-    return (
-      <>
-        <div>{count}</div>
-        <button onClick={() => this.modify(count + 1)}>Increment</button>
-      </>
-    );
-  }
+function App() {
+  const [count, setCount] = useState(0);
+  return (
+    <>
+      {count}
+      <button onClick={() => setCount(count + 1)}>Increment</button>
+      <button onClick={() => setCount(count - 1)}>Decrement</button>
+    </>
+  );
 }
 
 export default App;
