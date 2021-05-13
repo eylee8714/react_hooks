4f37a93a28cf343cf06af5b87caf9de003cd98da
eylee8714 <eylee8714@gmail.com>
2021-05-13 12:38:25 +0900

    Add useInput, useTabs example

--- hooks/useInput/useInput.js
+++ hooks/useInput/useInput.js
@@ -15,3 +15,39 @@ export const useInput = (initialValue, validator) => {
   };
   return { value, onChange };
 };
+
+// import React, { useState } from 'react';
+
+// const useInput = (initialValue, validator) => {
+//   const [value, setValue] = useState(initialValue);
+//   const onChange = (event) => {
+//     // console.log(event.target);
+//     const {
+//       target: { value },
+//     } = event;
+
+//     let willUpdate = true; // true니까 항상 update 될것이다.
+//     if (typeof validator === 'function') {
+//       willUpdate = validator(value);
+//     }
+//     if (willUpdate) {
+//       setValue(value);
+//     }
+//   };
+//   return { value, onChange };
+// };
+
+// const App = () => {
+//   // const maxLen = (value) => value.length <= 10;
+//   const maxLen = (value) => !value.includes('@'); // @를 포함하고있으면 업데이트를 하지않는다.
+//   const name = useInput('Mr.', maxLen);
+
+//   return (
+//     <>
+//       <h1>hi</h1>
+//       {/* <input placeholder="Name" value={name.value} onChange={name.onChange} /> */}
+//       <input placeholder="Name" {...name} />
+//     </>
+//   );
+// };
+// export default App;
--- hooks/useTabs/useTabs.js
+++ hooks/useTabs/useTabs.js
@@ -6,3 +6,39 @@ const useTabs = (initialTab, allTabs) => {
   }
   return { currentItem: allTabs[currentIndex], changeItem: setCurrentIndex };
 };
+
+// import React, { useState } from 'react';
+
+// const useTabs = (initialTab, allTabs) => {
+//   const [currentIndex, setCurrentIndex] = useState(initialTab);
+//   if (!allTabs || !Array.isArray(allTabs)) {
+//     // allTabs 가 아닐때, 배열이 아닐때 return 한다 : 빠져나온다.
+//     return;
+//   }
+//   return { currentItem: allTabs[currentIndex], changeItem: setCurrentIndex };
+// };
+
+// const content = [
+//   {
+//     tab: 'Section 1',
+//     content: "I'm the content of the Section 1",
+//   },
+//   {
+//     tab: 'Section 2',
+//     content: "I'm the content of the Section 2",
+//   },
+// ];
+
+// const App = () => {
+//   const { currentItem, changeItem } = useTabs(0, content); // useTabs(0) 이면 content(0) 를 얻는다.
+//   return (
+//     <>
+//       {content.map((section, index) => (
+//         <button onClick={() => changeItem(index)}>{section.tab}</button>
+//       ))}
+//       <div>{currentItem.content}</div>
+//     </>
+//   );
+// };
+
+// export default App;
