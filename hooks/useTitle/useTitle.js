0a459f5b762203d5b52458f9aa06c461c96638f2
eylee8714 <eylee8714@gmail.com>
2021-05-13 14:42:13 +0900

    useRef

---
+++ hooks/useTitle/useTitle.js
@@ -0,0 +1,29 @@
+const useTitle = (initialTitle) => {
+  const [title, setTitle] = useState(initialTitle);
+  const updateTitle = () => {
+    const htmlTitle = document.querySelector('title');
+    htmlTitle.innerText = title;
+  };
+  useEffect(updateTitle, [title]);
+  return setTitle;
+};
+
+//   import React, { useEffect, useState } from 'react';
+
+// const useTitle = (initialTitle) => {
+//   const [title, setTitle] = useState(initialTitle);
+//   const updateTitle = () => {
+//     const htmlTitle = document.querySelector('title');
+//     htmlTitle.innerText = title;
+//   };
+//   useEffect(updateTitle, [title]);
+//   return setTitle;
+// };
+
+// const App = () => {
+//   const titleUpdater = useTitle('Loading...');
+//   setTimeout(() => titleUpdater('Home'), 5000);
+//   return <div></div>;
+// };
+
+// export default App;
