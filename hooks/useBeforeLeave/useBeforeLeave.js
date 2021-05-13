5de7494edeb93cf78f20049dcd05a982627f80c3
eylee8714 <eylee8714@gmail.com>
2021-05-13 20:26:13 +0900

    useBeforeLeave

---
+++ hooks/useBeforeLeave/useBeforeLeave.js
@@ -0,0 +1,48 @@
+export const useBeforeLeave = (onBefore) => {
+  const handle = (event) => {
+    // console.log(event);
+    const { clientY } = event;
+    if (clientY <= 0) {
+      // 마우스가 위로 벗어날때만 onBefore 실행하기
+      onBefore();
+    }
+  };
+  useEffect(() => {
+    if (typeof onBefore !== 'function') {
+      return;
+    }
+    document.addEventListener('mouseleave', handle);
+    return () => document.removeEventListener('mouseleave', handle);
+  }, []);
+};
+
+// import React, { useEffect } from 'react';
+
+// const useBeforeLeave = (onBefore) => {
+//   const handle = (event) => {
+//     // console.log(event);
+//     const { clientY } = event;
+//     if (clientY <= 0) {
+//       // 마우스가 위로 벗어날때만 onBefore 실행하기
+//       onBefore();
+//     }
+//   };
+//   useEffect(() => {
+//     if (typeof onBefore !== 'function') {
+//       return;
+//     }
+//     document.addEventListener('mouseleave', handle);
+//     return () => document.removeEventListener('mouseleave', handle);
+//   }, []);
+// };
+// const App = () => {
+//   const beForeLife = () => console.log('pls dont leave');
+//   useBeforeLeave(beForeLife);
+//   return (
+//     <>
+//       <h1>hello</h1>
+//     </>
+//   );
+// };
+
+// export default App;
--- src/App.js
+++ src/App.js
@@ -1,26 +1,28 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 
-const usePreventLeave = (onLeaving) => {
-  const listener = (event) => {
-    event.preventDefault();
-    event.returnValue = ''; //이벤트에 대한 기본 작업이 금지되었는지 여부를 나타낸다
-    // returnValue는 deprecated 되었지만, 크롬에서 이걸 넣지 않으면 실행되지 않는다.
+const useBeforeLeave = (onBefore) => {
+  const handle = (event) => {
+    // console.log(event);
+    const { clientY } = event;
+    if (clientY <= 0) {
+      // 마우스가 위로 벗어날때만 onBefore 실행하기
+      onBefore();
+    }
   };
-  const enablePrevent = () => {
-    window.addEventListener('beforeunload', listener); // beforeunload는 window가 닫히기 전에 function이 실행되는것을 허락한다.
-  };
-  const disablePrevent = () => {
-    window.removeEventListener('beforeunload', listener);
-  };
-  return { enablePrevent, disablePrevent };
+  useEffect(() => {
+    if (typeof onBefore !== 'function') {
+      return;
+    }
+    document.addEventListener('mouseleave', handle);
+    return () => document.removeEventListener('mouseleave', handle);
+  }, []);
 };
-
 const App = () => {
-  const { enablePrevent, disablePrevent } = usePreventLeave();
+  const beForeLife = () => console.log('pls dont leave');
+  useBeforeLeave(beForeLife);
   return (
     <>
-      <button onClick={enablePrevent}>Protect</button>
-      <button onClick={disablePrevent}>UnProtect</button>
+      <h1>hello</h1>
     </>
   );
 };
